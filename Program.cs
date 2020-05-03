8ea9bd7eb011aab87853d9ed0f4751af4bccba8c
vasilGerchev <32429288+vasilGerchev@users.noreply.github.com>
2020-05-03 19:16:23 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05.ForeignLanguage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string countryName = Console.ReadLine();
+            switch (countryName)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");                 
+                    break;
+
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+        }
+    }
+}
