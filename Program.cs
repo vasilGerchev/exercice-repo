10389e1932981f8eac748ed040f19cd403238790
vasilGerchev <32429288+vasilGerchev@users.noreply.github.com>
2020-05-03 19:14:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace _04.MonthPrinter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int montsNumber = int.Parse(Console.ReadLine());
+            string  mont;
+            switch (montsNumber)
+            {
+                case 1:
+                    mont = "January";
+                    break;
+
+                case 2:
+                    mont = "February";
+                    break;
+
+                case 3:
+                    mont = "March";
+                    break;
+
+                case 4:
+                    mont = "April";
+                    break;
+
+                case 5:
+                    mont = "May";
+                    break;
+
+                case 6:
+                    mont = "June";
+                    break;
+
+                case 7:
+                    mont = "July";
+                    break;
+
+                case 8:
+                    mont = "August";
+                    break;
+
+                case 9:
+                    mont = "September";
+                    break;
+
+                case 10:
+                    mont = "October";
+                    break;
+
+                case 11:
+                    mont = "November";
+                    break;
+
+                case 12:
+                    mont = "December";
+                    break;
+
+               default:
+                    mont = "Error!";
+                    break;
+            }
+            Console.WriteLine(mont);
+        }
+      
+    }
+}
