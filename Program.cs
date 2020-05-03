5f4080c564c4e5a6885eef069bb7ad48572b8339
vasilGerchev <32429288+vasilGerchev@users.noreply.github.com>
2020-05-03 19:11:59 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _03.BackInThirtyMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int afterThirtyMinites = minutes + 30;
+
+            if (afterThirtyMinites > 59)
+            {
+                afterThirtyMinites -= 60;
+                hours++;
+              
+            }
+            if (hours > 23)
+            {
+                hours -= 24;
+            }
+            Console.WriteLine($"{hours}:{afterThirtyMinites:D2}");
+        }
+    
+    }
+}
