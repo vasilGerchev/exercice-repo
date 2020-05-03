cf4b6cfc74b9fb54e4288d7ff3604ab9167b28ff
vasilGerchev <32429288+vasilGerchev@users.noreply.github.com>
2020-05-03 19:09:54 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _12.NumberChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int num = int.Parse(Console.ReadLine());
+
+
+                Console.WriteLine("It is a number.");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid input!");
+               
+            }
+        }
+    }
+}
